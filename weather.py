c963e6cff4cd28b2296a452cb7ccc80454d2f68a
aliced <alice.duff5@gmail.com>
2023-09-04 14:10:12 +0100

    Removing multiprocessing logic, will use scheduling going forward. Added temporary block to update_weather() to pull from a pickled weather data (for running offline). Cleaned up unused code. (Did not git add before previous two commits)

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import datetime
+from datetime import datetime, timedelta, date
 import pytz
 import requests
 import json
 import pandas as pd
+import pickle
 
 # TODO add UV index, add cloudcover (low*0.7 + mid * 0.3) for p cloudy, add night vs day for sun/clear night and pcloudy day/pcloudy night, 
 # add heavy rain, add windy, 
@@ -14,25 +15,35 @@ class Weather():
         # print(json.dumps(weather_dict, indent=4))
 
     def update_weather(self) -> dict:
-        response = requests.get((
-        'https://api.open-meteo.com/v1/forecast?latitude=38.8951&longitude=-77.0364'
-        '&hourly=uv_index,is_day,cloudcover_low,cloudcover_mid,'
-        'apparent_temperature,precipitation_probability,precipitation,snowfall,'
-        'cloudcover,visibility,windspeed_10m,windgusts_10m&temperature_unit=fahrenheit'
-        '&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1'
-        )).json()
-        # print(json.dumps(response, indent=4))
+        # response = requests.get((
+        # 'https://api.open-meteo.com/v1/forecast?latitude=38.8951&longitude=-77.0364'
+        # '&hourly=uv_index,is_day,cloudcover_low,cloudcover_mid,'
+        # 'apparent_temperature,precipitation_probability,precipitation,snowfall,'
+        # 'cloudcover,visibility,windspeed_10m,windgusts_10m&temperature_unit=fahrenheit'
+        # '&windspeed_unit=mph&precipitation_unit=inch&forecast_days=2'
+        # )).json()
+        # # print(json.dumps(response, indent=4))
+
+        file = open('weather_dict_temp', 'rb')
+        response = pickle.load(file)
+        file.close()
+
         return response
 
-    def get_weather_dict(self):
-        return self.weather_dict
+    def get_weather_dict(self, day=-1) -> dict:
+        if day == -1:
+            return self.weather_dict
+        elif day == 0:
+            return weather_instance.get_weather_dict().get('hourly')[:23]
+        elif day == 1:
+            return weather_instance.get_weather_dict().get('hourly')[24:]
     
     def print_weather_dict(self):
         df = pd.DataFrame.from_dict(self.weather_dict.get('hourly'))
         print(df)
 
     def is_rain_above_percent(self, percent) -> bool:
-        current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+        current_time = datetime.now(pytz.timezone('US/Eastern'))
         hour = current_time.hour
 
         rain_probabilities_list = self.weather_dict.get('hourly').get('precipitation_probability')
@@ -47,5 +58,34 @@ class Weather():
     def get_daily_low_apparent_temp(self):
         return round(min(self.weather_dict.get('hourly').get('apparent_temperature')))
     
+if __name__ == "__main__":
+    weather_instance = Weather()
+    # weather_instance.print_weather_dict()
 
+    # now = datetime.now(pytz.timezone('US/Eastern'))
+
+    # if now.hour < 18:
+    #     weather_instance.get_weather_dict().get('hourly')[:18]
+    # else:
+    #     weather_instance.get_weather_dict().get('hourly')[18:18]
+
+#     row_time = pytz.timezone('US/Eastern').localize(dateutil.parser.parse(weather_instance.get_weather_dict().get('hourly').get('time')[26]))
+#     # now_local = datetime.now(pytz.timezone('US/Eastern'))
+#     today_at_6 = pytz.timezone('US/Eastern').localize(datetime.combine(datetime.now(pytz.timezone('US/Eastern')).date(), datetime.min.time()) + timedelta(hours=18))
+#     diffretiation = today_at_6 - row_time
+    
+    
+# # insertion_date = dateutil.parser.parse('2018-03-13T17:22:20.065Z')
+# # diffretiation = pytz.utc.localize(datetime.datetime.utcnow()) - insertion_date
+
+#     print(today_at_6)
+#     print(row_time)
+#     print(diffretiation)
+
+# if diffretiation.days>30:
+#     print "The insertion date is older than 30 days"
+
+# else:
+#     print "The insertion date is not older than 30 days"
+# #The insertion date is older than 30 days
             
